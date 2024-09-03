a3e60d52581b01f635eac581f8932f6d8debb5d5
Antonella <antonella.beti@gmail.com>
2024-09-03 15:22:15 -0300

    se termino la modularizacion del backend-jwt

---
+++ backend-jwt/src/app.js
@@ -0,0 +1,19 @@
+import express from "express";
+import cookieParser from "cookie-parser";
+import { router } from "./routes/authRoutes.js";
+import config from "./config.js";
+import cors from "cors";
+import morgan from "morgan";
+
+const app = express();
+
+app.use(cors());
+app.use(morgan("dev"));
+app.use(express.json());
+app.use(cookieParser());
+
+app.use(router);
+
+app.listen(config.PORT, () => {
+  console.log(`Corriendo en el puerto ${config.PORT}`);
+});
